204bf63e09fdac261e56fdf4b3f1e3fde288ef77
Artur <arturexpone@yandex.ru>
2020-05-24 12:33:30 +0300

    Update socket.js

--- src/api/socket.js
+++ src/api/socket.js
@@ -1,4 +1,4 @@
 import socketIO from 'socket.io-client'
 
 
-export const socket = socketIO('https://dry-mountain-25307.herokuapp.com:80', {secure: true});
+export const socket = socketIO('https://dry-mountain-25307.herokuapp.com:80');
